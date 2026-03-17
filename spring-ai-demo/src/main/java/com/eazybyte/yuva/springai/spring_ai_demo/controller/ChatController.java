41b8c89f8cc476e95d8ecc1a6c51dd6e75cc3fa5
Yuvaprakash N <yuvaprakashnv@gmail.com>
2026-03-17 12:48:31 +0530

    Role Messages Demo

--- spring-ai-demo/src/main/java/com/eazybyte/yuva/springai/spring_ai_demo/controller/ChatController.java
+++ spring-ai-demo/src/main/java/com/eazybyte/yuva/springai/spring_ai_demo/controller/ChatController.java
@@ -18,7 +18,19 @@ public class ChatController {
 
     @GetMapping("/chat")
     public String chat(@RequestParam String message){
-        return chatClient.prompt(message).call().content();
+
+        return chatClient
+                .prompt()
+                .system("""
+                                                You are an internal IT helpdesk assistant. Your role is to assist\s
+                                                employees with IT-related issues such as resetting passwords,\s
+                                                unlocking accounts, and answering questions related to IT policies.
+                                                If a user requests help with anything outside of these\s
+                                                responsibilities, respond politely and inform them that you are\s
+                                                only able to assist with IT support tasks within your defined scope.
+                        """)
+                .user(message)
+                .call().content();
     }
 
 }
