f82f61f916c6ec256bbc23e5b3bb2ed9ed8c1197
Yuvaprakash N <yuvaprakashnv@gmail.com>
2026-03-18 08:55:41 +0530

    Spring AI Chat options

--- spring-ai-demo/src/main/java/com/eazybyte/yuva/springai/spring_ai_demo/config/ChatClientConfig.java
+++ spring-ai-demo/src/main/java/com/eazybyte/yuva/springai/spring_ai_demo/config/ChatClientConfig.java
@@ -3,6 +3,7 @@ package com.eazybyte.yuva.springai.spring_ai_demo.config;
 import com.eazybyte.yuva.springai.spring_ai_demo.advisor.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +12,12 @@ import java.util.List;
 @Configuration
 public class ChatClientConfig {
 
+    ChatOptions chatOptions=ChatOptions.builder().temperature(0.3).maxTokens(100).stopSequences(List.of("END")).build();
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
 
         return chatClientBuilder
+                .defaultOptions(chatOptions)
                 .defaultAdvisors(List.of(new SimpleLoggerAdvisor(), new TokenUsageAuditAdvisor()))
                 .defaultSystem("""
                         You are an internal HR assistant. Your role is to help\s
--- spring-ai-demo/src/main/java/com/eazybyte/yuva/springai/spring_ai_demo/controller/PromptStuffingController.java
+++ spring-ai-demo/src/main/java/com/eazybyte/yuva/springai/spring_ai_demo/controller/PromptStuffingController.java
@@ -1,12 +1,15 @@
 package com.eazybyte.yuva.springai.spring_ai_demo.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -24,7 +27,9 @@ public class PromptStuffingController {
     @GetMapping("/prompt-stuffing")
     public String promptStuffing(@RequestParam("message") String message) {
         return chatClient
+
                 .prompt()
+                .options(ChatOptions.builder().temperature(1.0).maxTokens(100).stopSequences(List.of("END")).build())
                 .system(systemPromptTemplate)
                 .user(message)
                 .call().content();
