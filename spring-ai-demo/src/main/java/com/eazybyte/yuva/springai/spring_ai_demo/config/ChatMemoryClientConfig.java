456b17a2e16099c040d9e89fbeada7c4aa285f2c
Yuvaprakash N <yuvaprakashnv@gmail.com>
2026-03-19 11:34:10 +0530

    Chat memory: InMemoryChatMemory

--- spring-ai-demo/src/main/java/com/eazybyte/yuva/springai/spring_ai_demo/config/ChatMemoryClientConfig.java
+++ spring-ai-demo/src/main/java/com/eazybyte/yuva/springai/spring_ai_demo/config/ChatMemoryClientConfig.java
@@ -2,7 +2,10 @@ package com.eazybyte.yuva.springai.spring_ai_demo.config;
 
 import com.eazybyte.yuva.springai.spring_ai_demo.advisor.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +15,12 @@ import java.util.List;
 @Configuration
 public class ChatMemoryClientConfig {
 
-    @Bean
-    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+    @Bean(name = "chatMemoryChatClient")
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+        Advisor messageChatMemoryAdvisor= MessageChatMemoryAdvisor.builder(chatMemory).build();
 
         return chatClientBuilder
+                .defaultAdvisors(List.of(messageChatMemoryAdvisor, new SimpleLoggerAdvisor()))
                 .build();
     }
 }
--- spring-ai-demo/src/main/java/com/eazybyte/yuva/springai/spring_ai_demo/controller/ChatMemoryController.java
+++ spring-ai-demo/src/main/java/com/eazybyte/yuva/springai/spring_ai_demo/controller/ChatMemoryController.java
@@ -1,10 +1,17 @@
 package com.eazybyte.yuva.springai.spring_ai_demo.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -12,7 +19,7 @@ public class ChatMemoryController {
 
     private ChatClient chatClient;
 
-    public ChatMemoryController(ChatClient chatClient){
+    public ChatMemoryController(@Qualifier("chatMemoryChatClient") ChatClient chatClient){
         this.chatClient=chatClient;
     }
 
