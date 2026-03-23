0953e3c5671c5c529ba001ecce61c1873e94b304
Yuvaprakash N <yuvaprakashnv@gmail.com>
2026-03-23 11:56:57 +0530

    Tools calling

--- spring-ai-demo/src/main/java/com/eazybyte/yuva/springai/spring_ai_demo/config/TimeClientConfig.java
+++ spring-ai-demo/src/main/java/com/eazybyte/yuva/springai/spring_ai_demo/config/TimeClientConfig.java
@@ -1,16 +1,11 @@
 package com.eazybyte.yuva.springai.spring_ai_demo.config;
 
+import com.eazybyte.yuva.springai.spring_ai_demo.tools.TimeTools;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.MessageWindowChatMemory;
-import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
-import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
-import org.springframework.ai.rag.preretrieval.query.transformation.TranslationQueryTransformer;
-import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
-import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,10 +15,11 @@ import java.util.List;
 public class TimeClientConfig {
 
     @Bean(name = "timeChatClient")
-    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory, TimeTools timeTools) {
         Advisor messageChatMemoryAdvisor= MessageChatMemoryAdvisor.builder(chatMemory).build();
 
         return chatClientBuilder
+                .defaultTools(timeTools)
                 .defaultAdvisors(List.of(messageChatMemoryAdvisor, new SimpleLoggerAdvisor()))
                 .build();
     }
